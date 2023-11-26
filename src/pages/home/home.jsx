aa57b508d4bd2822dcc2d55f589d9e7ada6896bf
khowfsix <lyhongphat261202@gmail.com>
2023-11-27 00:04:38 +0700

    list test

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,29 @@
 import './home.css';
-import { Box } from '@material-ui/core';
+import { Box, List, ListItem } from '@material-ui/core';
+
+const something = [
+	{
+		name: 'c1',
+		price: 100,
+	},
+	{ name: 'c2', price: 200 },
+	{ name: 'c3', price: 300 },
+];
 
 export default function Home() {
-	return <Box className="home">This is home page</Box>;
+	return (
+		<>
+			<Box>This is Home page</Box>
+			<List>
+				{something.map((element) => (
+					<ListItem>
+						<Box>
+							<Box>name: {element.name}</Box>
+							<Box>price: {element.price}</Box>
+						</Box>
+					</ListItem>
+				))}
+			</List>
+		</>
+	);
 }
