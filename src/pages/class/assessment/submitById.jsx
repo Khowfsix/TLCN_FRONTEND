5d910f7e058261ade3816c1668c15033a2780d0c
khowfsix <lyhongphat261202@gmail.com>
2023-12-23 18:14:04 +0700

    grading

--- src/pages/class/assessment/submitById.jsx
+++ src/pages/class/assessment/submitById.jsx
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useLocation, useNavigate } from 'react-router-dom';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, TextField } from '@mui/material';
 
 import axios from '../../../apis/axiosConfig';
+import { toast } from 'react-toastify';
 
 const SubmitById = () => {
 	const [params] = useSearchParams();
 	const location = useLocation();
 	const navigate = useNavigate();
-	const [grade, setGrade] = useState(-1);
+	const [grade, setGrade] = useState(0);
+	const [submit, setSubmit] = useState(null);
 
 	const handleGradeSubmission = () => {
-		console.log(`Chấm điểm ${grade}/10 cho bài nộp ${submissionId} của học viên ${studentId}`);
+		console.log(submit);
+
+		if (grade >= 0 && grade <= 10) {
+			axios
+				.put(`studentAttemptAssessment/update/${params.get('submissionId')}`, submit)
+				.then((response) => {
+					if (response.status === 201 && response.data) {
+						toast.success('Đã chấm điểm', {
+							theme: 'dark',
+						});
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
 	};
 
-	const [submit, setSubmit] = useState(null);
+	const handleChangeGrade = (event) => {
+		setGrade(event.target.value);
+		setSubmit((prev) => ({
+			...prev,
+			grade: event.target.value,
+		}));
+	};
 
 	const fetchData = () => {
 		axios
@@ -46,15 +69,35 @@ const SubmitById = () => {
 
 				<Typography variant="h5">Nội dung bài nộp: </Typography>
 
-				{/* {submit && submi
-				)} */}
+				{submit &&
+					submit.submition.map((item) => {
+						if (item.type === 'content')
+							return (
+								<Typography variant="body1">
+									<Typography variant="overline">Nội dung: </Typography>
+									<br />
+									<div dangerouslySetInnerHTML={{ __html: item && item.value }} />
+								</Typography>
+							);
+						else
+							return (
+								<Typography variant="body1">
+									<Typography variant="overline">Link: </Typography> {item.value}
+								</Typography>
+							);
+					})}
 
-				<Typography variant="body1">
-					<Typography variant="overline">Nội dung: </Typography> aksjdflkas
-				</Typography>
-				<Typography variant="body1">
-					<Typography variant="overline">Link: </Typography>
-				</Typography>
+				<TextField
+					id="outlined-basic"
+					type="number"
+					label="Điểm"
+					variant="outlined"
+					value={grade}
+					onChange={handleChangeGrade}
+					inputProps={{
+						min: 0,
+						max: 10,
+					}}></TextField>
 				<Button variant="contained" onClick={handleGradeSubmission}>
 					Chấm điểm
 				</Button>
