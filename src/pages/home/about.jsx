8f14401b0b765b4b81988237155e92111c74b34c
khowfsix <lyhongphat261202@gmail.com>
2023-12-28 07:50:26 +0700

    fix

--- src/pages/home/about.jsx
+++ src/pages/home/about.jsx
@@ -2,36 +2,43 @@ import React from 'react';
 import { Typography, Avatar, Grid, Card, CardContent } from '@mui/material';
 
 export default function About() {
-	const projectDescription = 'Mô tả dự án: [Thêm mô tả dự án ở đây]';
-	const courseInfo = 'Thông tin môn: [Thêm thông tin môn ở đây]';
-	const instructor = 'Giảng viên hướng dẫn: [Tên giảng viên]';
-	const students = [
-		{ name: 'Sinh viên 1', avatar: '/path/to/avatar1.png' },
-		{ name: 'Sinh viên 2', avatar: '/path/to/avatar2.png' },
-	];
-
 	return (
 		<div>
 			<Typography variant="h4" gutterBottom>
 				Thông tin nhóm dự án
 			</Typography>
+			<h2>
+				TIỂU LUẬN CHUYÊN NGÀNH HỆ THỐNG THÔNG TIN
+				<br />
+				<b>ĐỀ TÀI:</b> SỬ DỤNG NGÔN NGỮ PYTHON VỚI THƯ VIỆN FRAMEWORK: FLASK VÀ REACTJS ĐỂ PHÁT TRIỂN HỆ THỐNG THÔNG TIN HỖ TRỢ QUẢN LÝ ĐÀO TẠO LẬP TRÌNH TRỰC TUYẾN
+			</h2>
 			<Card variant="outlined">
 				<CardContent>
 					<Typography variant="h6">Mô tả dự án:</Typography>
-					<Typography>{projectDescription}</Typography>
-					<Typography variant="h6">Thông tin môn:</Typography>
-					<Typography>{courseInfo}</Typography>
-					<Typography variant="h6">Giảng viên hướng dẫn:</Typography>
-					<Typography>{instructor}</Typography>
+					<Typography sx={{ mb: 1.5, textAlign: 'left' }}>
+						Đề tài tiểu luận chuyên ngành Hệ thống thông tin Sử dụng ngôn ngữ python với thư viện framework: Flask và Reactjs để phát triển hệ thống thông tin hỗ trợ quản lý đào tạo lập
+						trình trực tuyến có nhiều phân hệ khác nhưng do hạn chế về mặc thời gian, đề tài chỉ tập trung các phân hệ, bao gồm: Phân hệ học viên, phân hệ giảng viên, phân hệ nhân viên.
+						<p>Tiến trình thực hiện đồ án gồm các nội dung sau:</p>
+						<p>
+							+ Nội dung phân tích thiết kế hệ thống, bao gồm: Phân tích yêu cầu chức năng của các đối tượng người dùng, phân tích, thiết kế và cài đặt cơ sở dữ liệu, phân tích thiết kế
+							về giao diện người dùng
+						</p>
+						<p>+ Nội dung xây dựng hệ thống theo từng phân hệ, bao gồm: Lược đồ Sequence theo các chức năng, cấu trúc hệ thống, kết quả xây dựng đạt được.</p>
+						<p>+ Nội dung kiểm thử hệ thống theo từng đối tượng người dùng</p>
+						<p>+ Nội dung quá trình triển khai hệ thống theo từng đối tượng người dùng</p>
+					</Typography>
+					<Typography variant="h6">Môn: Tiểu luận chuyên ngành Hệ thống Thông tin</Typography>
+					<Typography variant="h6">Giảng viên hướng dẫn: Võ Xuân Thể</Typography>
+
 					<Typography variant="h6">Sinh viên:</Typography>
-					<Grid container spacing={2}>
-						{students.map((student, index) => (
-							<Grid item key={index}>
-								<Avatar alt={student.name} src={student.avatar} />
-								<Typography>{student.name}</Typography>
-							</Grid>
-						))}
-					</Grid>
+					<div>
+						<Grid item key={1}>
+							<Typography>Nguyên Ngọc Phát - 20110261</Typography>
+						</Grid>
+						<Grid item key={2}>
+							<Typography>Lý Hồng Phát - 20110692</Typography>
+						</Grid>
+					</div>
 				</CardContent>
 			</Card>
 		</div>
